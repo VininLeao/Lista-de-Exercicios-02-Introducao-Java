5204f76edb241b966adeb5356eb90f64d6cc7a50
VininLeao <104996741+VininLeao@users.noreply.github.com>
2023-08-05 17:20:00 -0300

    Exercício 7

---
+++ Ex07.java
@@ -0,0 +1,8 @@
+public class Ex07 {
+    public static void main(String[] args) {
+        for (int i = 3; i <= 100; i += 3) {
+            System.out.println(i + " dividido por 2 usando int é: " + i / 2);
+            System.out.println(i + " dividido por 2 usando double é: " + (double) i / 2);
+        }
+    }
+}
